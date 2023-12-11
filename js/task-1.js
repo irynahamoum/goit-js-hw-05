c175cb6c81acc869d27290801164e3fc07b2980b
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2023-12-11 15:19:57 +0100

    completed

---
+++ js/task-1.js
@@ -0,0 +1,79 @@
+// Напиши стрілочну функцію getUserNames(users), яка прийматиме один параметр users —
+//  масив об’єктів користувачів.Функція має повертати масив імен усіх користувачів
+//   (властивість name) із масиву users.
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її викликів.
+
+const getUserNames = (users) => users.map((user) => user.name);
+
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811,
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821,
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793,
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278,
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951,
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498,
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764,
+    },
+  ])
+);
+
+// ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
+// На що буде звертати увагу ментор при перевірці:
+// Оголошена змінна getUserNames
+// Змінній getUserNames присвоєна стрілочна функція з параметром (users).
+// Для перебирання параметра users використовується метод map()
+// Виклик функції із зазначеним масивом користувачів повертає масив
+// ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr",
+// "Blackburn Dotson", "Sheree Anthony"]
+// Виклик функції з випадковими, але валідними аргументами повертає правильне значення
+
+// function mcDonaldsWindow() {
+//   let order = 0;
+//   const getOrder = (handCallback) => {
+//     handCallback(order);
+//     order += 1;
+//   };
+//   return getOrder;
+// }
+// window.getOrder = mcDonaldsWindow();
+
+// function testCallback() {
+//   const price = 50;
+//   let amountOfOrders = 0;
+//   window.getOrder((orders) => {
+//     amountOfOrders = orders;
+//   });
+//   return price * amountOfOrders;
+// }
+// window.testCallback = testCallback;
